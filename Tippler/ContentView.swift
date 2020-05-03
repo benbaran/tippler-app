acb753d91f785d4b68383e95cfd66c0d1ee1488e
Ben Baran <ben.baran@gmail.com>
2020-05-03 07:58:05 -0500

    Initial Commit

---
+++ Tippler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tippler
+//
+//  Created by Ben Baran on 5/3/20.
+//  Copyright © 2020 Ben Baran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
